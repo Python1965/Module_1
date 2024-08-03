3b7a475f0a656c1ed84716778095f188b8720c28
Python1965 <python65@rambler.ru>
2024-08-03 17:11:46 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -13,9 +13,11 @@
 
 print("Введите произвольный текст:")
 my_string = input(">> ")
+print("Количество символов строки:", len(my_string))
 
 print(my_string.upper())
 print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
+
+my_string_new = print(my_string.replace(' ', ''))
+print(my_string_new[0])
+print(my_string_new[-1])
